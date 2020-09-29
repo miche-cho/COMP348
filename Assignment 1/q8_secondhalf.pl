6eedac30871e367940a8381f0386df907ed58069
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2020-09-29 17:56:27 -0400

    Update q8_secondhalf.pl

--- Assignment 1/q8_secondhalf.pl
+++ Assignment 1/q8_secondhalf.pl
@@ -7,3 +7,14 @@ separate([_|T], Size, SecondHalf):- length(T, Size1), (Size1 =< Size/2 -> Second
 
 /*  This functor generates the second half of a list*/
 second_half(L, X):- length(L, Size), separate(L, Size, X).
+/* 
+?- second_half([a,b,c,d,e,f,g,h], X)
+
+OUTPUT
+X = [e,f,g,h].
+
+?- second_half([d,e,f,g,h], X)
+
+OUTPUT
+X = [g,h].
+*/
