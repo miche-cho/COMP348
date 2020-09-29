33f8a3b2e19c56ba9e01426dd594817dfa85b937
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2020-09-29 17:42:43 -0400

    Update q6_fsm.pl

--- Assignment 1/q6_fsm.pl
+++ Assignment 1/q6_fsm.pl
@@ -15,3 +15,20 @@ transition(s4,d,s3).
 accept(L) :- start(S), path(Q, L).
 path(Q, [Lh | Lt]) :- transition(Q, X, QI), path(QI, Lt).
 path(Q,[]):- final(Q).
+/*
+part 2. verifying if the sequence "a a b" is accepted by the machine.
+?- accept([a,a,b]).
+
+OUTPUT:
+true
+
+part 3. Explanation
+
+Looking at the image of the machine, we know its initial and final state is S1. 
+The path defined by "a" transitions from S1 to S2.
+The path defined by "a" transitions from S2 to itself.
+The path defined by "b" transitions from S2 to S1.
+
+Since S1 is a final state, the final state machine can terminate there. 
+Thus, "a a b" is a valid sequence.
+*/
