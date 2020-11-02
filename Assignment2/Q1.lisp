8f8687d426b193d1b91f444ed43131c28a4ca1b3
Michelle <michelle.choi@mail.mcgill.ca>
2020-11-02 01:14:43 -0500

    Update Q1.lisp
    refactor

--- Assignment2/Q1.lisp
+++ Assignment2/Q1.lisp
@@ -11,7 +11,7 @@
 (defun sub-list (lst a b)
     (setf diff (- b a))   ; different btw to and from 
     (cond 
-        ((or(null lst)(null a)(null b) nil)) ; if not list, and a and b are all empty return nil
+        ((or(null lst)(null a)(null b)) nil)) ; if not list, and a and b are all empty return nil
         ((< b a) nil) ; return nil if a is bigger than b 
         ((< 1 a) (sub-list (cdr lst) (- a 1) b))  ; start a index a starting from 1 
         ((< 0 diff) (cons (car lst) (sub-list (cdr lst) a  (- diff 1) ))) ; add numbers from difference onwards
