4c45f47c0bb9ceb4b8e22e8fbd2ffd8a1d183d2d
miche-cho <michelle.choi@mail.mcgill.ca>
2020-11-02 00:46:03 -0500

    added comments and added unused function

--- Assignment2/Q1.lisp
+++ Assignment2/Q1.lisp
@@ -1,11 +1,23 @@
-(defun sub-list (lst a b)
-    (setf diff (- b a))
+; get the nth element (this is unused atm)
+(defun get-nth-el (lst n)
     (cond 
-        ((or(null lst)(null a)(null b) nil))
-        ((< b a) nil)
-        ((< 1 a) (sub-list (cdr lst) (- a 1) b))
-        ((< 0 diff) (cons (car lst) (sub-list (cdr lst) a  (- diff 1) )))
+        ((or (null lst) (< n 1)) nil)
+        ((equal n 1) (car lst))
+        (t  (get-n (cdr lst)(- n 1)))))
+
+
+
+
+(defun sub-list (lst a b)
+    (setf diff (- b a))   ; different btw to and from 
+    (cond 
+        ((or(null lst)(null a)(null b) nil)) ; if not list, and a and b are all empty return nil
+        ((< b a) nil) ; return nil if a is bigger than b 
+        ((< 1 a) (sub-list (cdr lst) (- a 1) b))  ; start a index a starting from 1 
+        ((< 0 diff) (cons (car lst) (sub-list (cdr lst) a  (- diff 1) ))) ; add numbers from difference onwards
         )
     )
 
+
+; testing
 (print(sub-list '(1 3 5 2 3) 2 2))
