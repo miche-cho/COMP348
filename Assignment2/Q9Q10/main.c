3c57c190fdbfd553c747cc99c19a262eb5fc8eec
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2020-11-04 16:07:13 -0500

    Add files via upload

---
+++ Assignment2/Q9Q10/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "selectionSortQ10.h"
+#include "selectionSortQ10.h"
+
+
+void print_array(int* arr, int size);
+
+int main() 
+{
+    int arr[] = {1, 4, 5 , 6, -1};
+    print_array(arr, 5);
+    int* m = findmin(arr, 5);
+
+    printf("The smallest value in this array is %d\n", *m);
+    selectionSort(arr,5);
+    print_array(arr, 5);
+    int c = 9;
+    int j = 23;
+    swap(&c, &j);
+    return 0;
+}
+
+//This just prints the contents of an array
+void print_array(int* arr, int size)
+{
+    printf("The array is now: ");
+    int i;
+    for (i = 0; i < size; i = i + 1)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
