95690b3199ed2890baa22fc98f6ea8e0544ad3ed
HWJFish <71451506+HWJFish@users.noreply.github.com>
2020-11-04 23:27:03 -0500

    removed some code to fix redefinition errors

--- Assignment2/Q9Q10/selectionSortQ10.h
+++ Assignment2/Q9Q10/selectionSortQ10.h
@@ -2,10 +2,14 @@
 //Version 2.0 it is for Q10
 #ifndef SELECTIONSORTQ10_H_
 #define SELECTIONSORTQ10_H_
-#include "selectionSortQ10.c"
 
-static int* findmin(int* arr, int size);
+
+
 void selectionSort(int* arr, int size);
-static void swap(int* x, int* y);
 
+/* removed code to fix redefinition errors
+#include "selectionSortQ10.c"
+static int* findmin(int* arr, int size);
+static void swap(int* x, int* y);
+*/
 #endif
