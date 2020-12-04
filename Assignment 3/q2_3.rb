2592ebe78e48150b6f45d44817dcee5381e2b8d4
miche-cho <michelle.choi@mail.mcgill.ca>
2020-12-03 20:45:25 -0500

    added error statements to all other shapes, fixed counter

--- Assignment 3/q2_3.rb
+++ Assignment 3/q2_3.rb
@@ -33,11 +33,21 @@ class Circle < Shape
     end
 
     def perimeter
-        return Math::PI * 2 * @radius
+        # raise exception if radius is not positive (Q2)
+        if (@radius < 0)
+            raise ArgumentError, 'Invalid Circle'
+        else
+            return Math::PI * 2 * @radius
+        end
     end
 
     def area
-        return Math::PI * @radius * @radius
+        # raise exception if radius is not positive (Q2)
+        if (@radius < 0)
+            raise ArgumentError, 'Invalid Circle'
+        else
+            return Math::PI * @radius * @radius
+        end
     end
 end
 #This is the class Rectangle
@@ -49,11 +59,21 @@ class Rectangle < Shape
     end
 
     def perimeter
-        return 2 * @height + 2 * @width
+        # raise exception if either height or width are not positive (Q2)
+        if (@height < 0 || @width < 0)
+            raise ArgumentError, 'Invalid Rectangle'
+        else
+            return 2 * @height + 2 * @width
+        end
     end
 
     def area
-        return @height * @width
+        # raise exception if either height or width are not positive (Q2)
+        if (@height < 0 || @width < 0)
+            raise ArgumentError, 'Invalid Rectangle'
+        else
+            return @height * @width
+        end
     end
 end
 
@@ -68,10 +88,6 @@ class Ellipse < Shape
         else
             @a = min
             @b = maj
-        end
-        # raise exception if either a or b are not positive (Q2)
-        if (@a < 0 || @b < 0)
-            raise ArgumentError, 'Invalid Ellipse'
         end
     end
     
@@ -112,36 +128,40 @@ File.readlines('input.txt').each do |line|
     # creates new object instance where element is capitalized
     shape = Shape.const_get(element[0].capitalize).new
     
-    # if the line (array) has more than one element 
-    if element.length>1
+    # if the line (array) has one or more element 
+    if element.length>0
         # since the head is the shape type, look at it to determine what to do 
         case element[0]
         
         # shape have undefined perimeter and area, only count
         when "shape"
-        @numShapes+=1
+            @numShapes+=1
         
         # rectange returns perimeter, area and count
         when "rectangle"
-        shape.instance_variable_set(:@height, element[1].to_f)
-        shape.instance_variable_set(:@width, element[2].to_f)
-        @numShapes+=1
-        @numRectangles+=1
-        
+            h = shape.instance_variable_set(:@height, element[1].to_f)
+            w = shape.instance_variable_set(:@width, element[2].to_f)
+            # checks if both height an width are not negative (otherwise it's invalid and not counted) (Q3)
+            if (h >= 0 && w >= 0)
+                @numShapes+=1
+                @numRectangles+=1
+            end
         # circle returns perimeter, area and count
         when "circle"
-        shape.instance_variable_set(:@radius, element[1].to_f)
-        @numShapes+=1
-        @numCircles+=1
-        
+            r = shape.instance_variable_set(:@radius, element[1].to_f)
+            # checks if radius not negative (otherwise it's invalid and not counted) (Q3)
+            if (r >= 0)
+                @numShapes+=1
+                @numCircles+=1
+            end
         # ellipse has condition for checking whether invalid,
         # has undefined perimeter, area and count
         when "ellipse"
-        a = shape.instance_variable_set(:@a, element[1].to_f)
-        b = shape.instance_variable_set(:@b, element[2].to_f)
-        @numShapes+=1
-            # checks if both a an b are not null (otherwise it's invalid and not counted) (Q3)
-            if (a > 0  && b > 0)
+            a = shape.instance_variable_set(:@a, element[1].to_f)
+            b = shape.instance_variable_set(:@b, element[2].to_f)
+            # checks if both a an b are not negative (otherwise it's invalid and not counted) (Q3)
+            if (a >= 0  && b >= 0)
+                @numShapes+=1
                 @numEllipses+=1
             end
         end
